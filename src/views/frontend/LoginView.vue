6a9575928053cbd973b45a245c5060b9965115b3
riponahmedjr <riponahmedjrtr@gmail.com>
2023-02-20 22:48:34 +0600

    Login done

--- src/views/frontend/LoginView.vue
+++ src/views/frontend/LoginView.vue
@@ -14,14 +14,15 @@
                             <div class="control-group">
                                 <input type="text" v-model="form.phone_number" class="form-control mt-3" placeholder="Your phone number" required="required" />
                                 <p class="help-block text-danger">
-                                    <span v-if="errors.phone_number">{{ errors.phone_number[0] }}</span>
+                                    <small v-if="errors.phone_number">{{ errors.phone_number[0] }}</small>
                                 </p>
                             </div>
                             <div class="control-group">
                                 <input type="password" v-model="form.password" class="form-control" placeholder="Your password"
                                     required="required" />
                                 <p class="help-block text-danger">
-                                    <span v-if="errors.password">{{ errors.password[0] }}</span>
+                                    <small v-if="errors.password">{{ errors.password[0] }}</small>
+                                    <small class="text-danger" v-if="not_match">{{ not_match }}</small>
                                 </p>
                             </div>
                             <div>
@@ -58,8 +59,8 @@ export default {
             this.$store.dispatch('LOGIN', this.form)
             .then(res => {
                 if (res.data.success) {
-                    this.showNotification('success', 'Login successfully');
-                    this.$router.push({name: 'Home'})
+                    // this.showNotification('success', 'Login successfully');
+                    this.$router.push({name: 'home'})
                 } else {
                     this.errors = res.data.msg;
                     if (typeof this.errors !== 'object') {

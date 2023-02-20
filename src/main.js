b60719c5880376b045b75a91a8d75aa17a5e6f26
riponahmedjr <riponahmedjrtr@gmail.com>
2023-02-20 23:49:03 +0600

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -5,17 +5,12 @@ import router from './router'
 import { createApp } from 'vue'
 import globals from './helpers/globals'
 
-
-
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 
-
 const app = createApp(App)
-
 
 app.config.globalProperties.showNotification = (type, message) => {
   globals.toastFire(type, message)
 }
-
 
 app.use(store).use(router).mount('#app')
